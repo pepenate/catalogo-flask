cc495e65d2e057086cb775d9b8744461d5d4efb8
pepenate <pedroandres0844@hotmail.com>
2025-12-08 21:40:25 -0500

    Leer DB_HOST/DB_* desde entorno

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 DB_CONFIG = {
     # En Railway leerá DB_HOST, DB_PORT, etc.
     # En tu PC, si no existen variables, usará los valores por defecto.
-    "host": os.getenv("DB_HOST", "127.0.0.1"),
-    "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "Pedrinche@2020"),
-    "database": os.getenv("DB_NAME", "catalogo_selles"),
+    "host": os.getenv("mysql.railway.internal", "127.0.0.1"),
+    "port": int(os.getenv("3306", 3306)),
+    "user": os.getenv("root", "root"),
+    "password": os.getenv("kAUENhRhQnNOhCduDBvJILnwKZEpnePL", "Pedrinche@2020"),
+    "database": os.getenv("railway", "catalogo_selles"),
 }
