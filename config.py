3079a84f7f097f850824e5d85f3c848aa63975a2
pepenate <pedroandres0844@hotmail.com>
2025-12-08 20:58:40 -0500

    Config DB lista para Railway

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 import os
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST"),
+    "host": os.getenv("mysql.railway.internal"),
     "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("root"),
+    "password": os.getenv("kAUENhRhQnNOhCduDBvJILnwKZEpnePL"),
+    "database": os.getenv("railway"),
 }
