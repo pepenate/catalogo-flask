efaa5b4881534b75b9c6fc76608cb52a302f3938
pepenate <pedroandres0844@hotmail.com>
2025-12-08 21:31:29 -0500

    Fix DB config for Railway

--- config.py
+++ config.py
@@ -2,9 +2,11 @@
 import os
 
 DB_CONFIG = {
-    "host": os.getenv("mysql.railway.internal"),
-    "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("root"),
-    "password": os.getenv("kAUENhRhQnNOhCduDBvJILnwKZEpnePL"),
-    "database": os.getenv("railway"),
+    # En Railway usará las variables MYSQLHOST, etc.
+    # En tu PC usará los valores por defecto (localhost).
+    "host": os.getenv("MYSQLHOST", "127.0.0.1"),
+    "port": int(os.getenv("MYSQLPORT", 3306)),
+    "user": os.getenv("MYSQLUSER", "root"),
+    "password": os.getenv("MYSQLPASSWORD", "Pedrinche@2020"),
+    "database": os.getenv("MYSQLDATABASE", "catalogo_selles"),
 }
